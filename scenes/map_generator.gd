c3d4436e80f51d28d2e831d75967ef433e64289c
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2024-12-17 15:18:19 +0000

    ADD: Tile selector component
    Can now detect which tile is being hovered (selected), even with overlapping area2Ds.
    
    Emits signal when selected tile changes.

--- scenes/map_generator.gd
+++ scenes/map_generator.gd
@@ -6,9 +6,19 @@ extends Node2D
 @onready var mountain: Mountain = $Mountain
 var tree_coverage_noise = FastNoiseLite.new()
 
+var height_matrix =[]
+
 
 func _ready() -> void:
+	initialize_height_matrix()
 	generate_map()
+
+
+func initialize_height_matrix() -> void:
+	for i in width:
+		height_matrix.append([])
+		for j in height:
+			height_matrix[i].append(0.0)
 
 
 func generate_map() -> void:
@@ -18,29 +28,30 @@ func generate_map() -> void:
 			var width_val = (1 / width) * i
 			var height_val = (1 / height) * j
 			var cell_noise_value: float = average(width_val, height_val) * white_noise_value
+			height_matrix[i][j] = cell_noise_value
 
 			if cell_noise_value <= 0.1:
-				mountain.get_child(0).set_cell(Vector2i(i - 16, j - 16), tree_coverage(Vector2i(i, j)), Vector2i(0, 0))
+				mountain.get_child(0).set_cell(Vector2i(i, j), tree_coverage(Vector2i(i, j)), Vector2i(0, 0))
 			elif cell_noise_value <= 0.14:
-				mountain.get_child(1).set_cell(Vector2i(i - 15, j - 15), tree_coverage(Vector2i(i, j)), Vector2i(0, 0))
+				mountain.get_child(1).set_cell(Vector2i(i, j), tree_coverage(Vector2i(i, j)), Vector2i(0, 0))
 			elif cell_noise_value <= 0.18:
-				mountain.get_child(2).set_cell(Vector2i(i - 14, j - 14), tree_coverage(Vector2i(i, j)), Vector2i(0, 0))
+				mountain.get_child(2).set_cell(Vector2i(i, j), tree_coverage(Vector2i(i, j)), Vector2i(0, 0))
 			elif cell_noise_value <= 0.22:
-				mountain.get_child(3).set_cell(Vector2i(i - 13, j - 13), tree_coverage(Vector2i(i, j)), Vector2i(0, 0))
+				mountain.get_child(3).set_cell(Vector2i(i, j), tree_coverage(Vector2i(i, j)), Vector2i(0, 0))
 			elif cell_noise_value <= 0.26:
-				mountain.get_child(4).set_cell(Vector2i(i - 12, j - 12), tree_coverage(Vector2i(i, j)), Vector2i(0, 0))
+				mountain.get_child(4).set_cell(Vector2i(i, j), tree_coverage(Vector2i(i, j)), Vector2i(0, 0))
 			elif cell_noise_value <= 0.276:
-				mountain.get_child(4).set_cell(Vector2i(i - 12, j - 12), tree_coverage(Vector2i(i, j)), Vector2i(1, 0))
+				mountain.get_child(4).set_cell(Vector2i(i, j), tree_coverage(Vector2i(i, j)), Vector2i(1, 0))
 			elif cell_noise_value <= 0.33:
-				mountain.get_child(5).set_cell(Vector2i(i - 11, j - 11), tree_coverage(Vector2i(i, j)), Vector2i(0, 0))
+				mountain.get_child(5).set_cell(Vector2i(i, j), tree_coverage(Vector2i(i, j)), Vector2i(0, 0))
 			elif cell_noise_value <= 0.4:
-				mountain.get_child(5).set_cell(Vector2i(i - 11, j - 11), tree_coverage(Vector2i(i, j)), Vector2i(1, 0))
+				mountain.get_child(5).set_cell(Vector2i(i, j), tree_coverage(Vector2i(i, j)), Vector2i(1, 0))
 			elif cell_noise_value <= 0.48:
-				mountain.get_child(6).set_cell(Vector2i(i - 10, j - 10), tree_coverage(Vector2i(i, j)), Vector2i(0, 0))
+				mountain.get_child(6).set_cell(Vector2i(i, j), tree_coverage(Vector2i(i, j)), Vector2i(0, 0))
 			elif cell_noise_value <= 0.58:
-				mountain.get_child(6).set_cell(Vector2i(i - 10, j - 10), tree_coverage(Vector2i(i, j)), Vector2i(1, 0))
+				mountain.get_child(6).set_cell(Vector2i(i, j), tree_coverage(Vector2i(i, j)), Vector2i(1, 0))
 			elif cell_noise_value <= 0.8:
-				mountain.get_child(7).set_cell(Vector2i(i - 9, j - 9), tree_coverage(Vector2i(i, j)), Vector2i(0, 0))
+				mountain.get_child(7).set_cell(Vector2i(i, j), tree_coverage(Vector2i(i, j)), Vector2i(0, 0))
 
 
 func average(value_1: float, value_2: float) -> float:
---
+++ scripts/components/tile_selector.gd
@@ -0,0 +1,44 @@
+class_name TileSelector
+extends Node2D
+
+signal current_cell_changed(coords)
+
+@export var enabled:= true
+@export var map_generator: MapGenerator
+@export var mountain: Mountain
+
+var mountain_layers: Array
+var mouse_pos
+var hovered_rids: Array = []
+
+
+func _ready() -> void:
+	mountain_layers = mountain.get_children()
+
+
+func _physics_process(_delta):
+	mouse_pos = get_global_mouse_position()
+
+	var params = PhysicsPointQueryParameters2D.new()
+	params.collide_with_areas = true
+	params.position = mouse_pos
+	var intersections = get_world_2d().direct_space_state.intersect_point(params)
+	if intersections == hovered_rids:
+		return
+	else:
+		hovered_rids = intersections
+		get_highest_cell_coords(hovered_rids)
+
+
+func get_highest_cell_coords(rids) -> void:
+	var highest_cell_coords = null
+
+	for rid in rids:
+		var selection = rid.collider.get_coords_for_body_rid(rid.rid)
+		if highest_cell_coords == null:
+			highest_cell_coords = selection
+		else:
+			if selection - highest_cell_coords > Vector2i(0,0):
+				highest_cell_coords = selection
+
+	current_cell_changed.emit(highest_cell_coords)
